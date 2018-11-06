7ea889dbb65654ce95d01df8f8de5a49f4cb5ae8
Scott Bergler <commandinghands@gmail.com>
2018-11-05 20:21:17 -0800

    add glyphicons and refactor scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // BUSINESS LOGIC
-
 function Song(title, duration) {
   this.title = title,
   this.duration = duration,
@@ -9,13 +8,16 @@ function Song(title, duration) {
 function Jukebox() {
   this.queue = [],
   this.queue.totalSongs = 0,
-  this.currentSong = [],
+  this.currentSong = "",
   this.counter = 1
 }
 
 Jukebox.prototype.addSong = function(song) {
-  this.queue.push(song);
+  debugger;
   song.id = this.assignId();
+  this.queue.push(song);
+  var songHtml = "<p>" + song.title + "</p>";
+  $("#displayQueue").append(songHtml);
 }
 
 Jukebox.prototype.assignId = function() {
@@ -34,11 +36,22 @@ Jukebox.prototype.grabId = function(inputID) {
 Jukebox.prototype.playThrough = function() {
   for (var i=0; i<this.queue.length; i++) {
     if (this.queue[i].id === this.counter) {
-      this.currentSong = [];
-      this.currentSong.push(this.queue[i]);
-      return this.counter += 1;
+      this.currentSong = this.queue[i];
     }
   }
+  return this.counter += 1;
+}
+
+Jukebox.prototype.displayQueue = function() {
+  var htmlForQueueDisplay = "";
+  var sondId;
+  var song;
+  for (var i=0; i<this.queue.length; i++) {
+    songIdNumber = this.queue[i].id;
+    song = this.queue[i].title;
+    htmlForQueueDisplay += "<p id=" + songIdNumber + ">" + song + "</p>"
+  }
+  return htmlForQueueDisplay;
 }
 
 // Jukebox.prototype.startPlay = function(counter) {
@@ -46,42 +59,35 @@ Jukebox.prototype.playThrough = function() {
 //   jukebox.currentSong.duration);
 // }
 
-var jukebox = new Jukebox;
-var newSong = new Song;
-
-var song1 = new Song('hello',30000,1);
-var song2 = new Song('goodbye',30000,2);
-var song3 = new Song('hey',30000,3);
-jukebox.addSong(song1);
-jukebox.addSong(song2);
-jukebox.addSong(song3);
-
 
 // USER INTERFACE LOGIC
 $(document).ready(function() {
+  var jukebox = new Jukebox;
 
-  $("#song1").text(jukebox.queue[0].title);
-  $("#song2").text(jukebox.queue[1].title);
-  $("#song3").text(jukebox.queue[2].title);
+  var song1 = new Song('hello',30000,1);
+  var song2 = new Song('goodbye',30000,2);
+  var song3 = new Song('hey',30000,3);
+  jukebox.addSong(song1);
+  jukebox.addSong(song2);
+  jukebox.addSong(song3);
 
+  var htmlForQueueDisplay = jukebox.displayQueue();
+  $("div#displayQueue").html(htmlForQueueDisplay);
 
   $("form#addSong").submit(function(event) {
     event.preventDefault();
-    var newSongTitle = $("input#title").val();
-    var newSongDuration = $("input#duration").val();
-
+    var songTitle = $("input#songTitle").val();
+    var songDuration = parseInt($("input#duration").val());
+    var song = new Song(songTitle, songDuration);
   })
 
   $("form#findSong").submit(function(event) {
     event.preventDefault();
   })
 
-$("#playNext").click(function() {
-  jukebox.playThrough();
-  console.log(jukebox.currentSong[0].title);
-  $("div#nowPlaying").text(jukebox.currentSong[0].title);
-  // $("div#displayQueue").html(currentQueue);
-  // $("div#history").text(queueHistory);
-})
+  $("#playNext").click(function() {
+    jukebox.playThrough();
+    $("div#nowPlaying").text(jukebox.currentSong.title);
+  })
 
 })
